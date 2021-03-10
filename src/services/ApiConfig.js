903bb3b105157489d9706064b8cb82e0bd07cc21
Aristotelis Palaiologopoulos <telis1@windowslive.com>
2021-03-10 15:03:11 +0100

    switched api

--- src/services/ApiConfig.js
+++ src/services/ApiConfig.js
@@ -1,5 +1,5 @@
 const url =
-  "https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=100&api_key=8ae55d463e1bf8d38b4a502ca47512f9b1dec21533ad9af7acb993e8ba952bc2";
+  "https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=100&api_key=b84c4247dc10717852e3f2b16549fad187cd6081b46c011a274f385e92c11e1b";
 
 export const getData = async () => {
   try {
